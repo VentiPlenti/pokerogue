413abe3f94c4fe6f03fa1a2f9bb2faba5edde8f0
Aleco <113533233+VentiPlenti@users.noreply.github.com>
2024-08-11 17:28:20 +0100

    Complete pokemon-info-container.ts

--- src/locales/ca_ES/pokemon-info-container.ts
+++ src/locales/ca_ES/pokemon-info-container.ts
@@ -1,9 +1,9 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const pokemonInfoContainer: SimpleTranslationEntries = {
-  "moveset": "Moveset",
-  "gender": "Gender:",
-  "ability": "Ability:",
-  "nature": "Nature:",
-  "form": "Form:"
+  "moveset": "Moviments:",
+  "gender": "Gènere:",
+  "ability": "Habilit:",
+  "nature": "Natur:",
+  "form": "Forma:"
 } as const;
