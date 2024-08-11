0ee323107a09771c3f75fafc867d5c869d7b4d0f
Aleco <113533233+VentiPlenti@users.noreply.github.com>
2024-08-11 20:39:07 +0100

    Complete fight-ui-handler.ts

--- src/locales/ca_ES/fight-ui-handler.ts
+++ src/locales/ca_ES/fight-ui-handler.ts
@@ -2,8 +2,8 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const fightUiHandler: SimpleTranslationEntries = {
   "pp": "PP",
-  "power": "Power",
-  "accuracy": "Accuracy",
-  "abilityFlyInText": " {{pokemonName}}'s {{passive}}{{abilityName}}",
-  "passive": "Passive ", // The space at the end is important
+  "power": "Dotència",
+  "accuracy": "Precisió",
+  "abilityFlyInText": " {{passive}}{{pokemonName}}\n{{abilityName}}",
+  "passive": "Passiva de ", // The space at the end is important
 } as const;
