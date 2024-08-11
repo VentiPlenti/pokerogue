16a7e1aac33388a9269f7a2da4bb77bd8151c300
Aleco <113533233+VentiPlenti@users.noreply.github.com>
2024-08-11 16:41:06 +0100

    Complete battler-tags.ts

--- src/locales/ca_ES/battler-tags.ts
+++ src/locales/ca_ES/battler-tags.ts
@@ -1,12 +1,12 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const battlerTags: SimpleTranslationEntries = {
-  "trappedDesc": "trapping",
-  "flinchedDesc": "flinching",
-  "confusedDesc": "confusion",
-  "infatuatedDesc": "infatuation",
-  "seedDesc": "seeding",
-  "nightmareDesc": "nightmares",
-  "ingrainDesc": "roots",
-  "drowsyDesc": "drowsiness",
+  "trappedDesc": "atrapant",
+  "flinchedDesc": "esgarrifant",
+  "confusedDesc": "confusió",
+  "infatuatedDesc": "enamorament",
+  "seedDesc": "sembrant",
+  "nightmareDesc": "malsons",
+  "ingrainDesc": "arrels",
+  "drowsyDesc": "somnolència",
 } as const;
